7ebcff5c9ce2b02ff4b25082eaeb9e3ec344fa20
gowdhaman-j <gowdhaman_j@yahoo.com>
2022-09-17 13:05:38 +0530

    Updated After Testing on AWS & Azure with RookNFS support
    Signed-off-by: gowdhaman-j <gowdhaman_j@yahoo.com>

--- 210-ibm-odf-storage/terraform/main.tf
+++ 210-ibm-odf-storage/terraform/main.tf
@@ -1,47 +1,12 @@
-module "cluster" {
-  source = "github.com/cloud-native-toolkit/terraform-ocp-login?ref=v1.6.0"
-
-  ca_cert = var.cluster_ca_cert
-  ca_cert_file = var.cluster_ca_cert_file
-  cluster_version = var.cluster_cluster_version
-  ingress_subdomain = var.cluster_ingress_subdomain
-  login_password = var.cluster_login_password
-  login_token = var.cluster_login_token
-  login_user = var.cluster_login_user
-  server_url = var.server_url
-  skip = var.cluster_skip
-  tls_secret_name = var.cluster_tls_secret_name
-}
-module "gitea" {
-  source = "github.com/cloud-native-toolkit/terraform-tools-gitea?ref=v0.5.0"
-
-  ca_cert = module.cluster.ca_cert
-  ca_cert_file = var.gitea_ca_cert_file
-  cluster_config_file = module.cluster.config_file_path
-  cluster_type = module.cluster.platform.type_code
-  instance_name = var.gitea_instance_name
-  instance_namespace = module.gitea_namespace.name
-  olm_namespace = module.olm.olm_namespace
-  operator_namespace = module.olm.target_namespace
-  password = var.gitea_password
-  username = var.gitea_username
-}
-module "gitea_namespace" {
-  source = "github.com/cloud-native-toolkit/terraform-k8s-namespace?ref=v3.2.3"
-
-  cluster_config_file_path = module.cluster.config_file_path
-  create_operator_group = var.gitea_namespace_create_operator_group
-  name = var.gitea_namespace_name
-}
 module "gitops_repo" {
   source = "github.com/cloud-native-toolkit/terraform-tools-gitops?ref=v1.21.0"
 
   branch = var.gitops_repo_branch
   debug = var.debug
-  gitea_host = module.gitea.host
-  gitea_org = module.gitea.org
-  gitea_token = module.gitea.token
-  gitea_username = module.gitea.username
+  gitea_host = var.gitops_repo_gitea_host
+  gitea_org = var.gitops_repo_gitea_org
+  gitea_token = var.gitops_repo_gitea_token
+  gitea_username = var.gitops_repo_gitea_username
   gitops_namespace = var.gitops_repo_gitops_namespace
   host = var.gitops_repo_host
   org = var.gitops_repo_org
@@ -56,7 +21,7 @@ module "gitops_repo" {
   username = var.gitops_repo_username
 }
 module "gitops-ibm-odf" {
-  source = "github.com/cloud-native-toolkit/terraform-gitops-ibm-odf?ref=v0.2.1"
+  source = "github.com/cloud-native-toolkit/terraform-gitops-ibm-odf?ref=v0.3.0"
 
   git_credentials = module.gitops_repo.git_credentials
   gitops_config = module.gitops_repo.gitops_config
@@ -66,7 +31,7 @@ module "gitops-ibm-odf" {
   server_name = module.gitops_repo.server_name
 }
 module "odf_namespace" {
-  source = "github.com/cloud-native-toolkit/terraform-gitops-namespace?ref=v1.11.2"
+  source = "github.com/cloud-native-toolkit/terraform-gitops-namespace?ref=v1.12.2"
 
   argocd_namespace = var.odf_namespace_argocd_namespace
   ci = var.odf_namespace_ci
@@ -76,10 +41,10 @@ module "odf_namespace" {
   name = var.odf_namespace_name
   server_name = module.gitops_repo.server_name
 }
-module "olm" {
-  source = "github.com/cloud-native-toolkit/terraform-k8s-olm?ref=v1.3.2"
+module "util-clis" {
+  source = "cloud-native-toolkit/clis/util"
+  version = "1.17.0"
 
-  cluster_config_file = module.cluster.config_file_path
-  cluster_type = module.cluster.platform.type_code
-  cluster_version = module.cluster.platform.version
+  bin_dir = var.util-clis_bin_dir
+  clis = var.util-clis_clis == null ? null : jsondecode(var.util-clis_clis)
 }
